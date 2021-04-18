e42ce884e11bfc11a89eb5484948befc6824aff9
Eugene Petrik <eugeniy.petrik@gmail.com>
2021-04-18 18:00:07 +0300

    Add JEST config

---
+++ jest.config.local.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+module.exports = {
+  testEnvironment: 'node',
+  reporters: [
+    'default',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'json',
+  ],
+  transform: { '^.+\\.jsx?$': 'babel-jest' },
+  testMatch: ['**/tests/**/*.test.js'],
+  globals: {
+    url: '',
+    testTimeout: 50000,
+  },
+  verbose: true,
+};
